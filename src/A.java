55138064d082e0ecd1985d8f4e1189ee25ca66f1
asavioti <angelo.savioti1@estudante.ufla.br>
2019-04-14 23:25:39 -0300

    Criação da Classe C

--- src/A.java
+++ src/A.java
@@ -1,4 +1,32 @@
-package PACKAGE_NAME;
-
 public class A {
+    private int A1;
+    private float A2;
+
+    public void MA1() {
+        System.out.println("MA1");
+    }
+
+    public void MA2() {
+        System.out.println("MA2");
+    }
+
+    public void MA3() {
+        System.out.println("Alteração da classe A a partir do clone");
+    }
+
+    public int getA1() {
+        return A1;
+    }
+
+    public float getA2() {
+        return A2;
+    }
+
+    public void setA1(int a1) {
+        A1 = a1;
+    }
+
+    public void setA2(float a2) {
+        A2 = a2;
+    }
 }
