dfa76ded8d264ac0b764e78f37552b5a177295d7
asavioti <angelo.savioti1@estudante.ufla.br>
2019-04-15 00:29:35 -0300

    atualizando classe D

---
+++ src/D.java
@@ -0,0 +1,12 @@
+public class D {
+    private double D1;
+    private long D2;
+
+    private void MD1() {
+        System.out.println("MD1");
+    }
+
+    public void MD2() {
+        System.out.println("MD2");
+    }
+}
